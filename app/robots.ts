86ffa85d4e4ebbf6ca2563c8ed843d39ac9ad018
orbitcontrol12345-gif <orbitcontrol12345@gmail.com>
2026-06-18 13:44:27 +0400

    Refactor robots rules structure and update sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -2,17 +2,14 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/api/',
-        '/admin/',
-      ],
-    },
-
-    sitemap: 'https://www.orbit-surplus.com/sitemap.xml',
-
-    host: 'https://www.orbit-surplus.com',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/admin/'],
+      },
+    ],
+    sitemap: 'https://orbit-surplus.com/sitemap.xml',
+    host: 'https://orbit-surplus.com',
   };
 }
