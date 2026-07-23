f19097631c7cc501d8279e875b3d588c6d5eebbf
orbitcontrol12345-gif <orbitcontrol12345@gmail.com>
2026-07-23 20:53:52 +0400

    Add EnterprisePreviewPage component

---
+++ app/enterprise-preview/page.tsx
@@ -0,0 +1,9 @@
+import HeroEnterprise from "@/components/enterprise/HeroEnterprise";
+
+export default function EnterprisePreviewPage() {
+  return (
+    <main className="min-h-screen bg-[#030712]">
+      <HeroEnterprise />
+    </main>
+  );
+}
