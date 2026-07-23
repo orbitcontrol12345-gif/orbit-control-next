87defa10de71a6ee9e9c3adef38fc09ae9d1f670
orbitcontrol12345-gif <orbitcontrol12345@gmail.com>
2026-07-23 16:00:53 +0400

    Add CtaSectionV2 import to page.tsx

--- app/design-preview/page.tsx
+++ app/design-preview/page.tsx
@@ -6,6 +6,7 @@ import FeaturedShowcase from '@/components/home/FeaturedShowcase';
 import StatisticsV2 from '@/components/home/StatisticsV2';
 import IndustriesV2 from '@/components/home/IndustriesV2';
 import GlobalPresenceV2 from '@/components/home/GlobalPresenceV2';
+import CtaSectionV2 from '@/components/home/CtaSectionV2';
 // سيتم إضافتها
 import CategoriesV2 from '@/components/home/CategoriesV2';
 
