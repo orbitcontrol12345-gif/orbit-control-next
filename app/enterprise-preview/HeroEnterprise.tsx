260ff4173482e0acd625c4ad9e27b172c2b9002a
orbitcontrol12345-gif <orbitcontrol12345@gmail.com>
2026-07-23 20:54:45 +0400

    Create HeroEnterprise.tsx

---
+++ app/enterprise-preview/HeroEnterprise.tsx
@@ -0,0 +1,9 @@
+export default function HeroEnterprise() {
+  return (
+    <section className="flex min-h-screen items-center justify-center bg-[#030712]">
+      <h1 className="text-5xl font-black text-white">
+        Orbit Control Enterprise
+      </h1>
+    </section>
+  );
+}
