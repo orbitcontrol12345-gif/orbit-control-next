5d171eecebd3bc8fcef1fd902e11bd07ad8a58e7
orbitcontrol12345-gif <orbitcontrol12345@gmail.com>
2026-07-31 21:16:37 +0400

    Update SITE_URL to use 'www' subdomain

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next';
 
-const SITE_URL = 'https://orbit-surplus.com';
+const SITE_URL = 'https://www.orbit-surplus.com';
 
 export default function robots(): MetadataRoute.Robots {
   return {
