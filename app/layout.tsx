fc90ceee7c4ff3d4ba912e25a0d5239043caf278
orbitcontrol12345-gif <orbitcontrol12345@gmail.com>
2026-07-31 21:05:06 +0400

    Update SITE_URL to use 'www' subdomain

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const inter = Inter({
   display: 'swap',
 });
 
-const SITE_URL = 'https://orbit-surplus.com';
+const SITE_URL = 'https://www.orbit-surplus.com';
 
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
