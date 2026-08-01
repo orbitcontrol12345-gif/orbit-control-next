e102d51dbf1f22fa572a3e7deaf6a6b4d4032a37
orbitcontrol12345-gif <orbitcontrol12345@gmail.com>
2026-08-01 14:48:22 +0400

    Update redirects in next.config.js
    Added additional redirects for about, contact, daily deals, and login paths.

--- next.config.js
+++ next.config.js
@@ -8,31 +8,44 @@ const nextConfig = {
     unoptimized: true,
   },
 
-  async redirects() {
-    return [
-      {
-        source: '/product-category/:path*',
-        destination: '/categories',
-        permanent: true,
-      },
-      {
-        source: '/shop/:path*',
-        destination: '/products',
-        permanent: true,
-      },
-      {
-        source: '/brand/:path*',
-        destination: '/brands',
-        permanent: true,
-      },
-    ];
-  },
+ async redirects() {
+  return [
+    {
+      source: '/product-category/:path*',
+      destination: '/categories',
+      permanent: true,
+    },
+    {
+      source: '/shop/:path*',
+      destination: '/products',
+      permanent: true,
+    },
+    {
+      source: '/brand/:path*',
+      destination: '/brands',
+      permanent: true,
+    },
 
-  async rewrites() {
-    return [
-     
-    ];
-  },
-};
-
+    {
+      source: '/about-us',
+      destination: '/about',
+      permanent: true,
+    },
+    {
+      source: '/contact-us',
+      destination: '/contact',
+      permanent: true,
+    },
+    {
+      source: '/daily-deals',
+      destination: '/products',
+      permanent: true,
+    },
+    {
+      source: '/login',
+      destination: '/',
+      permanent: true,
+    },
+  ];
+},
 module.exports = nextConfig;
