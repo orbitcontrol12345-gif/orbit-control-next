69ea1caaf06b5d815c47b7eeb1274ccc276db4f0
orbitcontrol12345-gif <orbitcontrol12345@gmail.com>
2026-08-01 14:51:07 +0400

    Refactor redirects in next.config.js

--- next.config.js
+++ next.config.js
@@ -8,44 +8,49 @@ const nextConfig = {
     unoptimized: true,
   },
 
- async redirects() {
-  return [
-    {
-      source: '/product-category/:path*',
-      destination: '/categories',
-      permanent: true,
-    },
-    {
-      source: '/shop/:path*',
-      destination: '/products',
-      permanent: true,
-    },
-    {
-      source: '/brand/:path*',
-      destination: '/brands',
-      permanent: true,
-    },
+  async redirects() {
+    return [
+      {
+        source: '/product-category/:path*',
+        destination: '/categories',
+        permanent: true,
+      },
+      {
+        source: '/shop/:path*',
+        destination: '/products',
+        permanent: true,
+      },
+      {
+        source: '/brand/:path*',
+        destination: '/brands',
+        permanent: true,
+      },
+      {
+        source: '/about-us',
+        destination: '/about',
+        permanent: true,
+      },
+      {
+        source: '/contact-us',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/daily-deals',
+        destination: '/products',
+        permanent: true,
+      },
+      {
+        source: '/login',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 
-    {
-      source: '/about-us',
-      destination: '/about',
-      permanent: true,
-    },
-    {
-      source: '/contact-us',
-      destination: '/contact',
-      permanent: true,
-    },
-    {
-      source: '/daily-deals',
-      destination: '/products',
-      permanent: true,
-    },
-    {
-      source: '/login',
-      destination: '/',
-      permanent: true,
-    },
-  ];
-},
+  async rewrites() {
+    return [];
+  },
+};
+
 module.exports = nextConfig;
